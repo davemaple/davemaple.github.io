dde27332d5412c8645ceb73d4a0473c7c6c550bb
David Maple <d@davemaple.com>
2016-09-09 00:31:33 -0400

    meta data

--- money-mutual/render.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-uglifycss css/bootstrap.min.css css/stylesheet.css css/media.css css/font-awesome.min.css > css/combined.min.css
-uglifyjs js/jquery.min.js js/bootstrap.min.js js/custom.js > js/combined.min.js
-optipng images/*.png
